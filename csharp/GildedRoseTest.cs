b9a0ead386d8d37e00d2a6b9ae246bb98ab3d168
Cameron Young <Cameronslatton@gmail.com>
2021-04-14 22:20:52 -0500

    Update GildedRoseTest.cs

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -13,7 +13,25 @@ namespace csharp
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [Test]
+        public void SellIn_When_Not_Sulfuras_Hand_Of_Ragnaros()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(-1, Items[0].SellIn);
+        }
+
+        [Test]
+        public void Quality_When_Not_Sulfuras_Hand_Of_Ragnaros()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(-1, Items[0].SellIn);
         }
     }
 }
